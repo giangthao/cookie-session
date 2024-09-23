e13641293ec70662f851d6ea02e0655e24225b68
giangthao <giangvtt@tasc.vn>
2024-09-23 18:50:01 +0700

    add ứ service

--- api/websocket.js
+++ api/websocket.js
@@ -1,38 +1,108 @@
-const WebSocket = require("ws");
-
-const PORT = 3001;
-
-
 // Create a WebSocket server
-const wss = new WebSocket.Server({ port: PORT });
+const express = require("express");
+const WebSocket = require("ws");
+const app = express();
+const PORT = 3001;
+const DEFAULT_CALLBACK = 6 * 1000;
+const wss = new WebSocket.Server({ noServer: true });
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+    credentials: true,
+  })
+);
+
+// Generate fake data
+function generateResponse() {
+  return {
+    lastUpdatedAt: new Date().toISOString(),
+    data: [
+      { code: "total_locate", value: Math.floor(Math.random() * 1000) },
+      { code: "total_catching", value: Math.floor(Math.random() * 1000) },
+      //  Locate
+      { code: "locate_manual_location_imei", value: Math.floor(Math.random() * 1000) },
+      { code: "locate_manual_location_not_imei", value: Math.floor(Math.random() * 1000) },
+      { code: "locate_manual_not_location_imei", value: Math.floor(Math.random() * 1000) },
+      { code: "locate_manual_not_location_not_imei", value: Math.floor(Math.random() * 1000) },
+      { code: "locate_auto_imei", value: Math.floor(Math.random() * 1000) },
+      { code: "locate_auto_not_imei", value: Math.floor(Math.random() * 1000) },
+      // Catching
+      { code: "catching_manual_location_imei", value: Math.floor(Math.random() * 1000) },
+      { code: "catching_manual_location_not_imei", value: Math.floor(Math.random() * 1000) },
+      { code: "catching_manual_not_location_imei", value: Math.floor(Math.random() * 1000) },
+      { code: "catching_manual_not_location_not_imei", value: Math.floor(Math.random() * 1000) },
+      { code: "catching_auto_imei", value: Math.floor(Math.random() * 1000) },
+      { code: "catching_auto_not_imei", value: Math.floor(Math.random() * 1000) },
+      //  System
+      {
+        code: "max_imei_per_second",
+        value: Math.floor(Math.random() * 1000),
+      },
+      {
+        code: "max_imsi_per_second",
+        value: Math.floor(Math.random() * 1000),
+      },
+      {
+        code: "total_response",
+        value: Math.floor(Math.random() * 1000),
+      },
+    ],
+  };
+}
 
 wss.on("connection", (ws) => {
-   console.log("Client connected");
+  console.log("Client connected");
 
-   // Send random numbers at the specified interval
-   const generateRandomNumbers = () => {
-      if (ws.readyState === WebSocket.OPEN) {
-         const randomValue = Math.floor(Math.random() * 100);
-         ws.send(randomValue.toString());
-      }
-   };
+  ws.on("message", (message) => {
+    const params = JSON.parse(message);
+    const { network, from, to } = params;
 
-   // Default interval is 1 second
-   //let timer = setInterval(generateRandomNumbers, DEFAULT_INTERVAL);
+    if (!["VIETTEL", "MOBILEPHONE", "VINAPHONE", "VIETNAMMOBILE"].includes(network.toUpperCase())) {
+      ws.send(JSON.stringify({ error: "Invalid network" }));
+      return;
+    }
+    console.log(`Received params: network = ${network}, from = ${from}, to = ${to}`);
 
-   ws.on("message", (message) => {
-      const newInterval = JSON.parse(message);
-      // Update the interval if received a new value from the client
-      console.log(`New interval: ${newInterval} second(s)`);
-      if (!isNaN(newInterval) && newInterval > 0) {
-        generateRandomNumbers();
-      }
-   });
+    const initialResponse = generateResponse();
+    ws.send(JSON.stringify(initialResponse));
 
-   ws.on("close", () => {
+    const interval = setInterval(() => {
+      const response = generateResponse();
+      ws.send(JSON.stringify(response)); 
+    }, DEFAULT_CALLBACK);
+
+    ws.on("close", () => {
+      ``;
       console.log("Client disconnected");
-      
-   });
+      clearInterval(interval);
+    });
+  });
+
+  setTimeout(() => {
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.send(JSON.stringify({ message: "Connection timed out" }));
+      ws.close();
+    }
+  }, DEFAULT_CALLBACK * 60);
 });
 
-console.log(`WebSocket server listening on port ${PORT}`);
+wss.on("error", (error) => {
+  if (error.code === "EADDRINUSE") {
+    console.error(`Port 3000 is already in use.`);
+  } else {
+    console.error("WebSocket server error:", error);
+  }
+});
+
+// Tạo HTTP server để tích hợp WebSocket
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// Chuyển đổi HTTP thành WebSocket
+server.on("upgrade", (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit("connection", ws, request);
+  });
+});
